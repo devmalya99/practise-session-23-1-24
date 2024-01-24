4c1f8620111da721c124630fddf8edd7d98a662f
devmalya99 <debmalyamazumdar99@gmail.com>
2024-01-24 13:44:38 +0530

    email added

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,51 @@
-import React from "react";
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import './App.css';
 function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
 
-  const [fullName,setFullName] =useState({
-    fName:"",
-    lName:"",
-  })
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    const {name,value}=e.target
-    
-    setFullName((prev)=>
+  const handleChange=(e)=>
+  {
+    const {name, value}=e.target
+    console.log(name, value)
+    setContact((prev)=>
     {
       return {...prev,[name]:value}
     })
-    // In your function handleChange, name is a variable that can either be 'fName' or 'lName' (based on the name property of the field that triggered the change).
-    //  So, you want to use it as a variable key, not a literal string key, hence you wrap it in square brackets: { [name]: value }.
-    //   This will correctly create { fName: "John" } or { lName: "Doe" } based on the input field.
   }
 
-  const handleSubmit =(e)=>
-  {
-    e.preventDefault();
-    console.log(fullName);
-    
-  }
 
   return (
     <div className="container">
-      <h1>Hello {fullName.fName} {fullName.lName}</h1>
-      <form onSubmit={handleSubmit}>
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+
         <input 
-        onChange={handleChange }
+        onChange={handleChange}
         name="fName" 
         placeholder="First Name" 
-        value={fullName.fName}
-        />
-      
-
-        <input
-        onChange={handleChange } 
-        name="lName" 
-        placeholder="Last Name" 
-        value={fullName.lName}
+        value={contact.fName}
         />
 
-        <button type="submit">Submit</button>
+        <input 
+        onChange={handleChange}
+        name="lName" placeholder="Last Name" 
+        value={contact.lName}
+        />
 
+        <input 
+        onChange={handleChange}
+        name="email" placeholder="Email" 
+        value={contact.email}
+        />
+        <button>Submit</button>
       </form>
     </div>
   );
